01430d76f0f254694df85e629d29502a71bdfd7f
Franco <phanfranco@yahoo.com>
2021-03-17 00:12:54 -0700

    Created routes folder with a test route using the controller. Also updated server.js file to test the route.

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(
+  process.env.MONGO_URI || "mongodb://localhost/GrooveIn",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("connected to mongodb");
+  }
+);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// custom routes
+app.use("/users", require("./routes/userRoutes"));
+
+// otherwise this route is used
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(PORT, () => console.log(`listening to http://localhost:${PORT}`));
