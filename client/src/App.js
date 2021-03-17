c6e4e49d122d3b098637161d8c0e74bbe5a1a2b8
Franco <phanfranco@yahoo.com>
2021-03-17 01:26:55 -0700

    Added pages folder with login,home and register pages. Cleared app.css file. Updated App.js file to be able to switch between pages with changing endpoints and created a function that checks if the user is logged in, if not, we are setting the token in local storage to empty. Created a hook to invoke the function.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
+  const checkLoggedIn = () => {
+    let token = localStorage.getItem("auth-token");
+    if (token === null) {
+      localStorage.setItem("auth-token", "");
+    }
+  };
+
+  useEffect(() => {
+    checkLoggedIn();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
