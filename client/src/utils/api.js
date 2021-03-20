2ffa328c9368905a8ed819d46d62ce5c3622cfd7
TruKat <kati.k220@gmail.com>
2021-03-20 14:18:29 -0600

    ADDED: upcoming events near you API functional

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,13 +1,17 @@
 import axios from "axios";
 
+
 function eventData() {
 
     const options = {
       method: 'GET',
-      url: 'https://seatgeek-seatgeekcom.p.rapidapi.com/events',
+      url: "https://api.seatgeek.com/2/events?client_id=MTkzMTk4OTF8MTYxNjI2MDQzOS42NTA5OTM2&client_secret=eaa2b57cbac19f713878974519146130e5a5d57fab22c28ff8315e014544aea0",
       headers: {
         'x-rapidapi-key': '102e0fee92mshced132a3d779a81p1b9eb5jsnb602debd05bb',
-        'x-rapidapi-host': 'seatgeek-seatgeekcom.p.rapidapi.com'
+        'x-rapidapi-host': 'seatgeek-seatgeekcom.p.rapidapi.com',
+        // 'Access-Control-Allow-Headers': 'x-rapidapi-host', 
+        // 'Access-Control-Allow-Origin': '*',
+        // 'Access-Control-Allow-Methods': 'GET'
       }
     };
     
@@ -17,5 +21,7 @@ function eventData() {
         console.error(error);
     });
 };
+
+
 
 export default eventData;
